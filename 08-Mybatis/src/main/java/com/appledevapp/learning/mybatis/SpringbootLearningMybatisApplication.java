6b38f4d629c3728d5e8cb5ed669c27d0fe296899
wubocheng <wubocheng@rfchina.com>
2018-02-13 08:57:47 +0800

    Mybatis General
    General Mapper And Class

---
+++ 08-Mybatis/src/main/java/com/appledevapp/learning/mybatis/SpringbootLearningMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.appledevapp.learning.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearningMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLearningMybatisApplication.class, args);
+	}
+}
